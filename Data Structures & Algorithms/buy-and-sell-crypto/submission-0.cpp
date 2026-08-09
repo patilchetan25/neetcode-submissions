b12241cf7b3fe1c9ae590c84ddd77c086769472a
Chetz <IAMRAJVEER25@GMAIL.COM>
2026-08-09 16:52:15 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0,r=1,profit=0,maxp=0;
+        while(r<prices.size()){
+            if(prices[l]<prices[r]){
+                profit=prices[r]-prices[l];
+                maxp=max(maxp,profit);
+            }
+            else{
+                l=r;
+            }
+            r++;
+        }
+        return maxp;
+        
+    }
+};
